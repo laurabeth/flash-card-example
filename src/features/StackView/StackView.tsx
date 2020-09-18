43af6eee017be13885891c853709f73a7e9e3e7d
veda <thisisillusion@gmail.com>
2020-09-18 04:59:14 -0400

    add some pseudocode

--- src/features/StackView/StackView.tsx
+++ src/features/StackView/StackView.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+// Should display the 'zoomed in' view of a stack of flash cards
+// One card at a time, title side first
+
 const StackView = () => {
   return (
     <div>
